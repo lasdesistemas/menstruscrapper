3f05fef4287f60e7b8d65c98e9fcbb9d8ea72616
Nayla Portas <naylaportas@gmail.com>
2019-03-05 17:01:53 -0300

    Loguea hora de inicio y fin del proceso

--- main/menstruscrapper.go
+++ main/menstruscrapper.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"fmt"
+	"time"
+
 	"github.com/lasdesistemas/menstruscrapper"
 	preciosclaros "github.com/lasdesistemas/menstruscrapper/precios-claros"
 )
 
 func main() {
 
+	fmt.Printf("Comienzo a buscar precios de toallitas y tampones a las %v", time.Now().Format("2006-01-02T15:04:05"))
 	cliente := preciosclaros.NewClient(&preciosclaros.Espera{}, &preciosclaros.PreciosClarosRestClient{})
 	scrapper := menstruscrapper.New(cliente)
 	scrapper.GenerarListaDePrecios()
+	fmt.Printf("Terminé de buscar precios de toallitas y tampones a las %v", time.Now().Format("2006-01-02T15:04:05"))
 }
